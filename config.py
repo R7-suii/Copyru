62896e8d8f375f0209061e3e5387a77d84900186
R7-suii <144661479+R7-suii@users.noreply.github.com>
2024-04-26 16:32:13 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,8 @@
 
 import os
 from os import getenv
+from prettyconf import Configuration
+from prettyconf.loaders import EnvFile, Environment
 # ---------------R---------------------------------
 API_ID = int(os.environ.get("API_ID"))
 # ------------------------------------------------
@@ -19,3 +21,16 @@ LOGGER_ID = int(os.environ.get("LOGGER_ID"))
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL")
 # ------------------------------------------------
+MESSAGE_DUMP = int(os.environ.get("MESSAGE_DUMP"))
+#---------------------------------------------------
+DB_URI = os.environ.get("DB_URI")
+#----------------------------------------------
+DB_NAME = os.environ.get("DB_NAME")
+#-------------------------------------------
+BDB_URI = config("BDB_URI",default=None)
+NO_LOAD = config("NO_LOAD", default="").split()
+PREFIX_HANDLER = config("PREFIX_HANDLER", default="/").split()
+SUPPORT_GROUP = config("SUPPORT_GROUP", default="gojo_bots_network")
+SUPPORT_CHANNEL = config("SUPPORT_CHANNEL", default="gojo_bots_network")
+WORKERS = int(config("WORKERS", default=16))
+TIME_ZONE = config("TIME_ZONE",default='Asia/Kolkata')
